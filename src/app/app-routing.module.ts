edafedb079c6ba40e9784e6c8453a98ca2b227f0
lionel <lioneltolosaarmas@gmail.com>
2019-01-07 17:09:39 +0100

    Routes and config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'movie/:id', component: MovieComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
